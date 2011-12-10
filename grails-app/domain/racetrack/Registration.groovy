df812d6210fd370647a5a1c585e7bb13aa8da010
Jay Zheng <jzheng@mobiquityinc.com>
2011-12-09 20:28:31 -0500

    first commit

---
+++ grails-app/domain/racetrack/Registration.groovy
@@ -0,0 +1,13 @@
+package racetrack
+
+class Registration {
+	static constraints = {
+			race()
+			runner()
+			paid()
+			dateCreated()
+	}
+	static belongsTo = [race:Race, runner:Runner]
+	Boolean paid
+	Date dateCreated
+}
